c79aa0710c4e88b2858decb7dd88767296879dca
Jason James <teretz.dev@gmail.com>
2024-04-02 16:28:27 -0500

    (no commit message provided)

--- pdfParse.cjs
+++ pdfParse.cjs
@@ -9,7 +9,7 @@ const parsePDF = async (filePath) => {
   pdfParser.loadPDF(filePath);
 
   return new Promise((resolve, reject) => {
-    pdfParser.on('pdfParser_dataError', errData => reject(errData.parserError)));
+    pdfParser.on('pdfParser_dataError', errData => reject(errData.parserError));
     pdfParser.on('pdfParser_dataReady', pdfData => {
       if (!pdfData || !pdfData.formImage || !pdfData.formImage.Pages) {
         reject(new Error('Invalid PDF structure.'));
